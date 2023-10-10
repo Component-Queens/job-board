6a996f56a6081c842d41a8c829c8bf45d9f0974d
jessicacliong <14395@coderacademy.edu.au>
2023-10-10 23:59:23 +1100

    feat: relocated img and restructured divs

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -3,25 +3,25 @@
 export default function FooterComponent() {
      return (
           <footer>
-               <div className="footer-greenhouse">
-                    <p className="greenhouse">Powered by
-                         <a href="https://www.greenhouse.com/" target="_blank" rel="noreferrer">
-                              <img 
-                                   className="greenhouse-logo" 
-                                   src={require("./greenhouse-logo.png")} 
-                                   alt={"Greenhouse logo"}
-                              />               
-                         </a>
-                    </p>
-               </div>
+                    <div className="footer-greenhouse">
+                         <p className="greenhouse">Powered by
+                              <a href="https://www.greenhouse.com/" target="_blank" rel="noreferrer">
+                                   <img 
+                                        className="greenhouse-logo" 
+                                        src={require("../assets/images/greenhouse-logo.png")} 
+                                        alt={"Greenhouse logo"}
+                                   />               
+                              </a>
+                         </p>
+                    </div>
 
-               <div className="footer-privacy">
-                    <p className="privacy-policy">Read our
-                         <a className="privacy-policy-link" 
-                         href="https://www.greenhouse.com/privacy-policy">Privacy Policy
-                         </a>
-                    </p>
-               </div>
+                    <div className="footer-privacy">
+                         <p className="privacy-policy">Read our
+                              <a className="privacy-policy-link" 
+                              href="https://www.greenhouse.com/privacy-policy">Privacy Policy
+                              </a>
+                         </p>
+                    </div>
           </footer>
      );
 }
