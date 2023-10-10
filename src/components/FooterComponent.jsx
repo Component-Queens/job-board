54e27bf6cfb51414db193ddbf2fa469d510818c0
jessicacliong <14395@coderacademy.edu.au>
2023-10-11 01:50:54 +1100

    feat: footer styling as per figma components outline

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -2,26 +2,26 @@
 
 export default function FooterComponent() {
      return (
-          <footer>
-                    <div className="footer-greenhouse">
-                         <p className="greenhouse">Powered by
-                              <a href="https://www.greenhouse.com/" target="_blank" rel="noreferrer">
-                                   <img 
-                                        className="greenhouse-logo" 
-                                        src={require("../assets/images/greenhouse-logo.png")} 
-                                        alt={"Greenhouse logo"}
-                                   />               
-                              </a>
-                         </p>
-                    </div>
+          <div className="footer">
+               <div className="footer-greenhouse">
+                    <p className="greenhouse">Powered by
+                         <a href="https://www.greenhouse.com/">
+                              <img 
+                                   className="greenhouse-logo" 
+                                   src={require("../assets/images/greenhouse-logo.png")} 
+                                   alt={"Greenhouse logo"}
+                              />               
+                         </a>
+                    </p>
+               </div>
 
-                    <div className="footer-privacy">
-                         <p className="privacy-policy">Read our
-                              <a className="privacy-policy-link" 
-                              href="https://www.greenhouse.com/privacy-policy">Privacy Policy
-                              </a>
-                         </p>
-                    </div>
-          </footer>
+               <div className="footer-privacy">
+                    <p className="privacy-policy">Read our
+                         <a className="privacy-policy-link" 
+                         href="https://www.greenhouse.com/privacy-policy">Privacy Policy
+                         </a>
+                    </p>
+               </div>
+          </div>
      );
 }
