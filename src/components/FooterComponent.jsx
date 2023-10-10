23fa036ff724aa1095912fc7444814d4d760528a
jessicacliong <14395@coderacademy.edu.au>
2023-10-10 22:35:36 +1100

    feat: inline greenhouse img

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -3,24 +3,26 @@
 export default function FooterComponent() {
      return (
           <footer>
-               <div className="footer-greenhouse">
-                    <p className="greenhouse">Powered by
-                         <a href="https://www.greenhouse.com/" target="_blank" rel="noreferrer">
-                              <img 
-                                   className="greenhouse-logo" 
-                                   src={require("./greenhouse-logo.png")} 
-                                   alt={"Greenhouse logo"}
-                              />               
-                         </a>
-                    </p>
-               </div>
+               <div className="footer">
+                    <div className="footer-greenhouse">
+                         <p className="greenhouse">Powered by
+                              <a href="https://www.greenhouse.com/" target="_blank" rel="noreferrer">
+                                   <img 
+                                        className="greenhouse-logo" 
+                                        src={require("./greenhouse-logo.png")} 
+                                        alt={"Greenhouse logo"}
+                                   />               
+                              </a>
+                         </p>
+                    </div>
 
-               <div className="footer-privacy">
-                    <p className="privacy-policy">Read our
-                         <a className="privacy-policy-link" 
-                         href="https://www.greenhouse.com/privacy-policy">Privacy Policy
-                         </a>
-                    </p>
+                    <div className="footer-privacy">
+                         <p className="privacy-policy">Read our
+                              <a className="privacy-policy-link" 
+                                   href="https://www.greenhouse.com/privacy-policy">Privacy Policy
+                              </a>
+                         </p>
+                    </div>
                </div>
           </footer>
      );
