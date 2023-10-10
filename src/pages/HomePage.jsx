449d47d822b7b865c27822c2c752a29729e948dc
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-10-10 19:21:37 +1100

    add: css for drop list

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,27 @@
+
 import JobTable from "../components/JobTable";
+import LocationDropList from "../components/Location";
 
 export default function HomePage(){
 
     return(
         <div className="all-jobs">
+            <div className="jobfilters">
+                <div className="search-box filter-item">
+                    <input type='text' placeholder='Search...' />
+                </div>
+                <div className="departments filter-item">
+                    <p>Departments</p>
+                </div>
+                <LocationDropList />
+            </div>
 
             {/* Search box component */}
             {/* Search All Departments Drop List */}
             {/* Search all location Drop List */}
             <JobTable></JobTable>
+            
+            
         </div>
     )
 }
